2165281671e5f3f4d9fdfba5c7c967c626e40162
SX-Yue <yue-sx22@mails.tsinghua.edu.cn>
2026-08-17 20:53:47 +0800

    add git_available_agent_English_ver_context project files

---
+++ deepseek_api_test/git_available_agent_English_ver_context/run_agent.bat
@@ -0,0 +1,16 @@
+@echo off
+
+REM ===============================================================================
+REM INSTRUCTIONS:
+REM 1. Run this .bat file in the current directory where the .bat file is located.
+REM 2. The second command is currently commented out. If you want to use it or run 
+REM    it from another directory, you must modify the paths in the second command 
+REM    to point to the correct PATH\TO\main_agent on your system.
+REM ===============================================================================
+
+REM Run the agent in the current directory where the .bat file is located
+REM "%~dp0.venv\Scripts\python.exe" "%~dp0main_agent.py" 
+
+REM Run the agent in another directory
+ "D:\projects\AI-Learning-Repository\deepseek_api_test\git_available_agent_English_ver_context\.venv\Scripts\python.exe" ^
+ "D:\projects\AI-Learning-Repository\deepseek_api_test\git_available_agent_English_ver_context\main_agent.py"
